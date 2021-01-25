5193d05a4395050765257d5b521450d12f518b8b
toutouff <antonincharpentier00@gmail.com>
2021-01-25 17:33:59 +0100

    réécriture de ScrapperProduct en poo

--- ScrapperProduct.py
+++ ScrapperProduct.py
@@ -1,23 +1,37 @@
 import requests
 from bs4 import BeautifulSoup
 
-url = 'http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html'
+class Page(object):
+    def __init__(self, url):
+        self.url = url
 
-reponse = requests.get(url)
+class ProductPage(Page):
 
-if reponse.ok:
+    def reponse(self):
+        reponse = requests.get(self.url)
+        self.soup = BeautifulSoup(reponse.text, features='html.parser')
+        return reponse
 
-    soup = BeautifulSoup(reponse.text, features='html.parser')
-    title = soup.find('h1').text
-    table = soup.find('table', class_='table table-striped')
-    td = table.find_all('td')
-    upc = td[0].text
-    priceeExTax = td[2].text
-    priceIncTax = td[3].text
-    nbrAiv = td[5].text
-    productPageUrl = url
-    productdescription = ''
-    body = soup.find('body', class_='default')
-    allP = soup.findAll('p')
-    productdescription = allP[3].text
-    print(productdescription)
+    def getTitle(self):
+        title = self.soup.find('h1').text
+        self.title = title
+
+    def getInfo(self):
+        self.reponse()
+        table = self.soup.find('table',class_='table table-striped')
+        td = table.findAll('td')
+        self.upc = td[0].text
+        self.priceeExTax = td[2].text
+        self.priceIncTax = td[3].text
+        self.nbrAiv = td[5].text
+        self.getTitle()
+        self.getProductDescription()
+
+    def getProductDescription(self):
+        allP = self.soup.findAll('p')
+        self.productdescription = allP[3].text
+
+
+productpage = ProductPage("http://books.toscrape.com/catalogue/a-light-in-the-attic_1000/index.html")
+productpage.getInfo()
+print(productpage.productdescription)
